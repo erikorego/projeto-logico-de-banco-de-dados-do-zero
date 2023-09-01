835077d0db9ce376b3392b3d9e1a55feebdf9748
erikorego <erikorego@outlook.com>
2023-09-01 10:15:35 -0300

    Updated data sample and Queries. readMe update

--- Queries.sql
+++ Queries.sql
@@ -1,1 +1,58 @@
 USE repair_shop;
+
+-- Lista de Clientes Pessoa física:
+SELECT 
+    CONCAT(first_name, ' ', surname) AS 'Nome do cliente'
+FROM
+    clients
+WHERE
+    NOT (cpf = 'NULL')
+ORDER BY first_name;
+
+-- Lista de Clientes Pessoa Jurídica:
+SELECT 
+    corporate_name AS 'Nome do cliente'
+FROM
+    clients
+WHERE
+    cnpj <> 'NULL'
+ORDER BY corporate_name;
+
+-- Lista de Funcionários e Cargos:
+SELECT 
+    CONCAT(first_name, ' ', surname) AS Funcionário,
+    position AS Cargo
+FROM
+    employee
+ORDER BY Cargo , Funcionário;
+
+-- Mecãnicos Ordenados por Ordem de Serviços Executadas:
+SELECT 
+    CONCAT(first_name, ' ', surname) AS Mecânico,
+    COUNT(idOS_employee) AS Quantidade
+FROM
+    os
+        INNER JOIN
+    employee ON (idOS_employee = idEmployee)
+GROUP BY Mecânico
+ORDER BY Quantidade DESC;
+
+-- Serviço mais utilizado:
+SELECT 
+    service_type AS Serviço, COUNT(service_type) AS QTD
+FROM
+    os
+GROUP BY Serviço
+ORDER BY QTD DESC;
+
+-- Clientes PF mais asssíduos de 2023:
+SELECT 
+    CONCAT(first_name, ' ', surname) AS Cliente,
+    COUNT(os_date > '2022-12-31') AS Visitas
+FROM
+    clients
+        INNER JOIN
+    os ON idClient = idOS_client
+GROUP BY Cliente
+HAVING Cliente <> 'NULL'
+ORDER BY Visitas DESC;
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # projeto-logico-de-banco-de-dados-do-zero
+
+_TABELAS_ :
+-------------------
+1. *CLIENTES* -> **clients**:
+    lista de todos os clientes que já fizeram algum serviço na oficina. Contém informações de veículo e identificação e classificação em PF ou PJ;
+2. *Peças* -> **part**: representa o estoque de peças presente na oficina
+3. *Funcionário* -> **employee**: Lista de funcionários na oficina, bem como seus cargos;
+4. *Ordens de Serviço* -> **os**: Contem os dados de todas as ordens de serviço: valores, quem executou o serviço, o cliente atendido, o valor do serviço;
+5. *peças por ordem de serviço* -> **parts_per_os**:representa a quantidade de peças usadas em uma determinada ordem de serviço.
